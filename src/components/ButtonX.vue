8a8c9249bf5fdcf00444b4a4fadcd5fb81497956
Tzur Almog <t.almog@kontakt.io>
2021-11-08 07:55:59 -0500

    Too much mess, cnat work with prettier

---
+++ src/components/ButtonX.vue
@@ -0,0 +1,15 @@
+<template>
+    <button> Add Task</button>
+</template>
+
+<script>
+
+
+export default {
+    name: 'ButtonX',
+    props: {
+        title: String,
+        
+    },
+}
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,31 @@
+/* eslint-disable */
+<template>
+    <header>
+        <h1>
+            {{ title }}
+        </h1>
+    </header>
+</template>
+
+<script>
+import ButtonX from './ButtonX'
+export default{
+    name: 'HeaderX',
+    props: {
+        title: String,
+        
+    },
+    components:{
+        ButtonX
+    },
+}
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
